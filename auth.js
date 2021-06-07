d161334841b847633dc70c1307a7791d25dcf5c5
StphnLwnga <boywilder99@gmail.com>
2021-06-07 14:30:40 +0200

    take 2

---
+++ auth.js
@@ -0,0 +1,100 @@
+const passport = require('passport');
+const ObjectID = require('mongodb').ObjectID;
+const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
+const bcrypt = require('bcrypt');
+
+module.exports = (app, myDatabase) => {
+	passport.serializeUser((user, done) => {
+		done(null, user._id);
+	});
+
+	passport.deserializeUser((id, done) => {
+		myDatabase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+			done(null, doc);
+		});
+	});
+
+	passport.use(new LocalStrategy(
+		(username, password, done) => {
+			myDatabase.findOne({ username }, (err, user) => {
+				console.log('User ' + username + ' attempted to log in.');
+				if (err) return done(err);
+				if (!user || !bcrypt.compareSync(password, user.password)) return done(null, false);
+				return done(null, user);
+			})
+		}
+	));
+	
+	passport.use(new GitHubStrategy({
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: 'https://boilerplate-advancednode.stlwanga.repl.co/auth/github/callback'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+			console.log(profile);
+			//Database logic here with callback containing our user object
+			myDatabase.findOneAndUpdate(
+				{ id: profile.id },
+				{
+					$setOnInsert: {
+						id: profile.id,
+						name: profile.username || 'John Doe',
+						photo: profile.photos[0].value || '',
+						email: Array.isArray(profile.emails)
+							? profile.emails[0].value
+							: 'No public email',
+						created_on: new Date(),
+						provider: profile.provider || ''
+					},
+					$set: {
+						last_login: new Date()
+					},
+					$inc: {
+						login_count: 1
+					}
+				},
+				{ upsert: true, new: true },
+				(err, doc) => {
+					return cb(null, doc.value);
+					// console.log(doc)
+				}
+			);
+  	}
+  ));
+
+	// passport.use(new GitHubStrategy({
+  //     clientID: process.env.GITHUB_CLIENT_ID,
+  //     clientSecret: process.env.GITHUB_CLIENT_SECRET,
+  //     callbackURL: 'https://boilerplate-advancednode.stlwanga.repl.co/auth/github/callback'
+  //   },
+  //   function (accessToken, refreshToken, profile, cb) {
+  //     console.log(profile);
+  //     myDatabase.findOneAndUpdate(
+	// 			{ id: profile.id },
+	// 			{
+	// 				$setOnInsert: {
+	// 					id: profile.id,
+	// 					name: profile.displayName || 'John Doe',
+	// 					photo: profile.photos[0].value || '',
+	// 					email: Array.isArray(profile.emails)
+	// 						? profile.emails[0].value
+	// 						: 'No public email',
+	// 					created_on: new Date(),
+	// 					provider: profile.provider || ''
+	// 				},
+	// 				$set: {
+	// 					last_login: new Date()
+	// 				},
+	// 				$inc: {
+	// 					login_count: 1
+	// 				}
+	// 			},
+	// 			{ upsert: true, new: true },
+	// 			(err, doc) => {
+	// 				return cb(null, doc.value);
+	// 			}
+	// 		);
+  //   }
+  // ));
+}
---
+++ server.js
@@ -0,0 +1,123 @@
+'use strict';
+// require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const myDB = require('./connection');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const session = require('express-session');
+const passport = require('passport');
+const routes = require('./routes');
+const auth = require('./auth');
+
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const passportSocketIo = require('passport.socketio');
+const cookieParser = require('cookie-parser');
+const MongoStore = require('connect-mongo')(session);
+const URI = process.env.MONGO_URI;
+const store = new MongoStore({ url: URI });
+
+app.set('views', path.join(__dirname, './views/pug'));
+app.set('view engine', 'pug');
+
+fccTesting(app); //For FCC testing purposes
+
+app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+	secret: process.env.SESSION_SECRET,
+	resave: true,
+	saveUninitialized: true,
+	cookie: { secure: false, },
+	key: 'express.sid',
+	store,
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// app.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', '*');
+//   res.header('Access-Control-Allow-Headers', 'Content-Type');
+//   res.header('Access-Control-Expose-Headers', '*')
+//   next();
+// })
+
+io.use(
+	passportSocketIo.authorize({
+		cookieParser,
+		key: 'express.sid',
+		secret: process.env.SESSION_SECRET,
+		store,
+		success: onAuthorizeSuccess,
+		fail: onAuthorizeFail
+	})
+);
+
+myDB(async client => {
+	const myDatabase = await client.db('fcc').collection('qa-ane');
+
+	auth(app, myDatabase);
+
+	routes(app, myDatabase);
+
+	let currentUsers = 0;
+	io.on('connection', socket => {
+		++currentUsers;
+		io.emit(
+			'user', 
+			{
+				name: socket.request.user.name,
+				currentUsers,
+				connected: true,
+			}
+		); // console.log(socket.request);
+		currentUsers > 0 && console.log(`user ${socket.request.user.name} connected`);
+
+		socket.on('chat message', message => {
+			io.emit(
+				'chat message', 
+				{ 
+					name: socket.request.user.name, 
+					message, 
+				}
+			);
+		})
+
+		socket.on('disconnect', () => {
+			console.log('user disconnected');
+			--currentUsers;
+			io.emit(
+				'user', 
+				{
+					name: socket.request.user.name,
+					currentUsers,
+					connected: true,
+				}
+			);
+		})
+	});
+
+	const PORT = process.env.PORT || 3000;
+	http.listen(PORT, () => {
+		console.log('Listening on port ' + PORT);
+	});
+}).catch(e => {
+	app.route('/').get((req, res) => {
+		res.render('index', { title: e, message: 'Unable to login' });
+	})
+});
+
+function onAuthorizeSuccess(data, accept) {
+	console.log('successful connection to socket.io');
+	accept(null, true);
+}
+
+function onAuthorizeFail(data, message, error, accept) {
+	if (error) throw new Error(message);
+  console.log('failed connection to socket.io:', message);
+  accept(null, false);
+}
