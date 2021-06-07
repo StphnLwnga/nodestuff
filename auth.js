2959ab77541b079db0d5fa5393109476b0c7383d
StphnLwnga <boywilder99@gmail.com>
2021-06-07 14:59:59 +0200

    take 2

--- auth.js
+++ auth.js
@@ -30,7 +30,7 @@ module.exports = (app, myDatabase) => {
 	passport.use(new GitHubStrategy({
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: 'https://boilerplate-advancednode.stlwanga.repl.co/auth/github/callback'
+      callbackURL: process.env.GITHUB_CBK,
     },
     function(accessToken, refreshToken, profile, cb) {
 			console.log(profile);
